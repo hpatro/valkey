62e0d8ede98d8c64fe9e4e418eb880658fef66df
Harkrishn Patro <harkrisp@amazon.com>
2025-06-14 04:27:07 +0000

    Fix engine start
    Signed-off-by: Harkrishn Patro <harkrisp@amazon.com>

--- tests/unit/cluster/cluster-debug-msg-extension.tcl
+++ tests/unit/cluster/cluster-debug-msg-extension.tcl
@@ -6,8 +6,13 @@ proc debug_msg_sum_reached {} {
         set msgmap [R $i DEBUG CLUSTER-MSG-GET]
         set sum 0
         foreach {id val} $msgmap {
+            if {$val eq {}} {
+                continue
+            }
             incr sum $val
         }
+        puts "Node $i: sum=$sum"
+        flush stdout
         if {$sum != $nodecount} {
             return 0
         }
@@ -15,7 +20,13 @@ proc debug_msg_sum_reached {} {
     return 1
 }
 
-start_cluster 3 3 {tags {external:skip cluster}} {
+start_cluster 100 100 {tags {external:skip cluster}} {
+
+    test "Cluster is up" {
+        puts "Waiting for cluster to be up..."
+        wait_for_cluster_state ok
+    }
+
     test "Debug message propagation" {
         set nodecount [llength $::servers]
         set start_time [clock milliseconds]
