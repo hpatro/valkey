178a24c0446faed4174a5d31f88ffa4b79f37b38
Harkrishn Patro <bunty.hari@gmail.com>
2026-03-11 20:55:20 -0700

    Test cluster I/O offload stats for both io-threads on and off
    Signed-off-by: Harkrishn Patro <bunty.hari@gmail.com>

--- tests/unit/cluster/cluster-io-offload-stats.tcl
+++ tests/unit/cluster/cluster-io-offload-stats.tcl
@@ -6,12 +6,21 @@ test "Cluster should start ok" {
     wait_for_cluster_state ok
 }
 
-test "Cluster I/O offload stats are present in INFO stats for all nodes" {
+test "Cluster I/O offload stats reflect io-threads configuration" {
+    # Determine if io-threads are active (>1 means offload is enabled)
+    set io_threads [lindex [R 0 config get io-threads] 1]
+
     for {set j 0} {$j < 3} {incr j} {
         set info [R $j info stats]
-        # With default io-threads=1, reads fall back to synchronous path
-        assert_equal 0 [getInfoProperty $info cluster_io_reads_offloaded]
-        assert {[getInfoProperty $info cluster_io_sync_fallbacks] > 0}
+        set reads_offloaded [getInfoProperty $info cluster_io_reads_offloaded]
+        set sync_fallbacks [getInfoProperty $info cluster_io_sync_fallbacks]
+
+        if {$io_threads > 1} {
+            assert {$reads_offloaded > 0}
+        } else {
+            assert {$sync_fallbacks > 0}
+        }
+
         assert_equal 0 [getInfoProperty $info cluster_io_async_closed_links]
         assert_equal 0 [getInfoProperty $info cluster_io_queued_inbound_packets]
     }
